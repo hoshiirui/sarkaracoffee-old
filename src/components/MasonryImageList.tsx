40cddcdc3e6d488cbf16ae38bd52f857070492e2
hoshiirui <shibaharumii@gmail.com>
2023-09-04 16:56:50 +0800

    adjusting image link to by id

--- src/components/MasonryImageList.tsx
+++ src/components/MasonryImageList.tsx
@@ -1,77 +1,41 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import { Grid } from '@mui/material';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import { Grid } from "@mui/material";
 
-export default function MasonryImageList() {
+interface MasonryImageListProps {
+  id: number | undefined;
+}
+
+export default function MasonryImageList(props: MasonryImageListProps) {
+  const itemData = [];
+  const basePath = `/img/${props.id}/imglist/`;
+  const numberOfImages = 12;
+  for (let i = 1; i <= numberOfImages; i++) {
+    const imgLink = `${basePath}${i}.JPG`;
+    itemData.push(imgLink);
+  }
   return (
     <Grid item xs={12} md={4}>
-        <Box sx={{ width: 420, height: 450, overflowY: 'scroll' }}>
+      <Box sx={{ width: 420, height: 450, overflowY: "scroll" }}>
         <ImageList variant="masonry" cols={3} gap={8}>
-            {itemData.map((item) => (
-            <ImageListItem key={item.img}>
+          {itemData.map(
+            (
+              imgLink // Updated this line
+            ) => (
+              <ImageListItem key={imgLink}>
                 <img
-                src={`${item.img}?w=248&fit=crop&auto=format`}
-                srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                alt={item.title}
-                loading="lazy"
+                  src={`${imgLink}?w=248&fit=crop&auto=format`}
+                  srcSet={`${imgLink}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                  alt=""
+                  loading="lazy" // Removed alt attribute since 'item.title' is no longer available
                 />
-            </ImageListItem>
-            ))}
+              </ImageListItem>
+            )
+          )}
         </ImageList>
-        </Box>
+      </Box>
     </Grid>
   );
 }
-
-const itemData = [
-  {
-    img: `/img/6/imglist/1.JPG`,
-    title: 'Bed',
-  },
-  {
-    img: `/img/6/imglist/2.JPG`,
-    title: 'Books',
-  },
-  {
-    img: `/img/6/imglist/3.JPG`,
-    title: 'Sink',
-  },
-  {
-    img: `/img/6/imglist/4.JPG`,
-    title: 'Kitchen',
-  },
-  {
-    img: `/img/6/imglist/5.JPG`,
-    title: 'Blinds',
-  },
-  {
-    img: `/img/6/imglist/6.JPG`,
-    title: 'Chairs',
-  },
-  {
-    img: `/img/6/imglist/7.JPG`,
-    title: 'Laptop',
-  },
-  {
-    img: `/img/6/imglist/8.JPG`,
-    title: 'Doors',
-  },
-  {
-    img: `/img/6/imglist/9.JPG`,
-    title: 'Coffee',
-  },
-  {
-    img: `/img/6/imglist/10.JPG`,
-    title: 'Storage',
-  },
-  {
-    img: `/img/6/imglist/11.JPG`,
-    title: 'Candle',
-  },
-  {
-    img: `/img/6/imglist/12.JPG`,
-    title: 'Coffee table',
-  },
-];
