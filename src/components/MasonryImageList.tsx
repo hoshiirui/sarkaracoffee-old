afe2f86f08be1580ee339748a5865e8438097de5
hoshiirui <shibaharumii@gmail.com>
2023-09-04 14:58:09 +0800

    testing the masonry-image-list with image datas

--- src/components/MasonryImageList.tsx
+++ src/components/MasonryImageList.tsx
@@ -27,51 +27,51 @@ export default function MasonryImageList() {
 
 const itemData = [
   {
-    img: 'https://images.unsplash.com/photo-1549388604-817d15aa0110',
+    img: `/img/6/imglist/1.JPG`,
     title: 'Bed',
   },
   {
-    img: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
+    img: `/img/6/imglist/2.JPG`,
     title: 'Books',
   },
   {
-    img: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
+    img: `/img/6/imglist/3.JPG`,
     title: 'Sink',
   },
   {
-    img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
+    img: `/img/6/imglist/4.JPG`,
     title: 'Kitchen',
   },
   {
-    img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
+    img: `/img/6/imglist/5.JPG`,
     title: 'Blinds',
   },
   {
-    img: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
+    img: `/img/6/imglist/6.JPG`,
     title: 'Chairs',
   },
   {
-    img: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
+    img: `/img/6/imglist/7.JPG`,
     title: 'Laptop',
   },
   {
-    img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
+    img: `/img/6/imglist/8.JPG`,
     title: 'Doors',
   },
   {
-    img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
+    img: `/img/6/imglist/9.JPG`,
     title: 'Coffee',
   },
   {
-    img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
+    img: `/img/6/imglist/10.JPG`,
     title: 'Storage',
   },
   {
-    img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
+    img: `/img/6/imglist/11.JPG`,
     title: 'Candle',
   },
   {
-    img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
+    img: `/img/6/imglist/12.JPG`,
     title: 'Coffee table',
   },
 ];
